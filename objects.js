0620550fa359fa3c0d1ae2893365464dc7337bc5
Nikhil Gugad <nikhilgugad111@gmail.com>
2025-09-11 05:02:57 +0000

    learned new objecs methods

--- objects.js
+++ objects.js
@@ -1,15 +1,15 @@
 //        *** Objects are most important in JavaScript  ****          //
 
 // declaring a object
-const jsUser = {
-    name: "hitesh",
-    age : "18",
-    location : "jodhpur",
-    email : "someone@example.com",
-    isLoggedIn : false,
-    lastLoginDays : ['monday', 'saturday'],
-    "full name" : "Hitesh Choudhary" // this value cant be accessed by jsUser.fullname.
-}; // object literals
+// const jsUser = {
+//     name: "hitesh",
+//     age : "18",
+//     location : "jodhpur",
+//     email : "someone@example.com",
+//     isLoggedIn : false,
+//     lastLoginDays : ['monday', 'saturday'],
+//     "full name" : "Hitesh Choudhary" // this value cant be accessed by jsUser.fullname.
+// }; // object literals
 
 // ways of accessing a object value
 
@@ -52,5 +52,52 @@ const jsUser = {
 // declaring a object using constructor
 // Object.create(); // Singleton
 
+// const tinderUser = new Object();
+
+// tinderUser.id = "123abc";
+// tinderUser.name = "Sammy";
+// tinderUser.isLoggedIn = false;
 
 
+// console.log(Object.keys(tinderUser)); // return an array of all the keys of a object.
+// console.log(Object.values(tinderUser)); // return an array of all the values of a object.
+// console.log(Object.entries(tinderUser)); // return an array of all the individual key value pairs as a array.
+
+// console.log(tinderUser.hasOwnProperty("isLoggedIn")); // returs a boolean on the basis of if it has the specified property.
+
+
+
+
+
+
+// const regularUser = {
+//     email : "some@gmail.com",
+//     fullname : {
+//         userFullName : {
+//             firstName : "Hitesh",
+//             lastName : "Choudhary"
+//         }
+//     }
+// }
+
+// console.log(regularUser);
+
+
+// const obj1 = {
+//     1 : "a",
+//     2 : "b"
+// }
+
+// const obj2 = {
+//     3 : "c",
+//     4 : "d"
+// }
+
+// // const obj3 = {obj1, obj2}; 
+// // console.log(obj3);
+
+// // const obj3 = Object.assign({}, obj1, obj2);
+// // console.log(obj3);
+
+// const obj3 = {...obj1, ...obj2};
+// console.log(obj3);
