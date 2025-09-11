167aae363be86e124e78aa0c371aaa00e8aefd7c
Nikhil Gugad <nikhilgugad111@gmail.com>
2025-09-11 05:17:47 +0000

    learned how to destructure objects

---
+++ object-destructure.js
@@ -0,0 +1,17 @@
+const course = {
+    courseName : "js in hindi",
+    price : 999,
+    courseInstructor : "Hitesh"
+}
+
+// const {courseInstructor} = course; // now we can call this value by only courseInstructor variable.
+
+
+// console.log(courseInstructor);
+
+// const {courseInstructor : inst} = course; // we can also use another name to refer this.
+// console.log(inst);
+
+
+//  JSON = "javascript object notation".
+
